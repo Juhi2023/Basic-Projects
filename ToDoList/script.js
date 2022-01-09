c07707a372cf6a2791c9b21fb245b19af73a0543
Juhi Sahu <juhisahu2023@gmail.com>
2022-01-09 18:30:08 +0530

    Project 1

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,69 +1,91 @@
 let addBtn = document.getElementById('add_btn');
-        addBtn.addEventListener('click', addTask);
-        let ulTag= document.getElementById('listofTask');
+let inputTaken = document.getElementById('inputTaken');
+let ulTag= document.getElementById('listofTask');
+let itemList=JSON.parse(localStorage.getItem("list")) || [];
 
-        function addTask(e){
-            
-            if(ulTag.children[0].className =="emptylist")
-            {
-                ulTag.children[0].remove();
-            }
-            
-            let currentBtn = e.currentTarget;
-            let currentData = currentBtn.previousElementSibling;
-            let currentinput = currentData.value;
+window.onload=function()
+{
+    display();
+}
 
-            if(currentData.value=="")
-            {
-                alert("Please enter a task.")
-            }
-            else{
-                let newLi = document.createElement('li');
-                // newLi.classList.add('list-group-item');
-                newLi.className="list-group-item d-flex justify-content-between"
-                newLi.innerHTML = `<input class="form-check-input me-3" type="checkbox" value="" id="flexCheckDefault">
-                                <h6 class="flex-grow-1">${currentinput}</h6>
-                                <button id ="add_btn" class="btn btn-outline-light bg-warning"  type="button" id="button-add" onclick="editTask(this)">Edit</button>
-                                <button id ="add_btn" class="btn btn-outline-light bg-danger"  type="button" id="button-add" onclick="removeTask(this)">Remove</button>`;
-
-                ulTag.appendChild(newLi);
-            }
-            
-        }
-    
-    function removeTask(currElement){
-        currElement.parentElement.remove();
-        if(ulTag.children.length<=0)
-        {
-            let newH = document.createElement('h5');
-            newH.classList.add('emptylist');
-            newH.textContent = 'Nothing is Here';
-            ulTag.appendChild(newH);
-        }
+addBtn.addEventListener('click', ()=>
+{
+    if(inputTaken.value=="")
+    {
+        alert("Please enter a task.")
     }
-
-    function editTask(currElement){
-        if(currElement.textContent =='Done')
-        {
-            currElement.textContent='Edit';
-            let elementData=currElement.previousElementSibling.value;
-            let newInput= document.createElement('h6');
-            newInput.textContent=elementData;
-            newInput.className='flex-grow-1';
-            currElement.parentElement.replaceChild(newInput, currElement.previousElementSibling);
-
-
+    else
+    {
+        let l={
+            task: inputTaken.value,
+            done: false
         }
-        else{
-            currElement.textContent="Done";
-            let elementData = currElement.previousElementSibling.textContent;
-            let newInput = document.createElement('input');
-            newInput.type="text";
-            newInput.className = "form-control";
-            newInput.placeholder="";
-            newInput.value = elementData;
-            currElement.parentElement.replaceChild(newInput, currElement.previousElementSibling);
-        }
+        itemList.push(l);
+        localStorage.setItem("list", JSON.stringify(itemList));
+        display();
+    }
+});
+
+function display()
+{
+    let listofTask="";
+    for (let i=0; i< itemList.length; i++)
+    {
+        listofTask+=`<li class="list-group-item d-flex justify-content-between"><input id="c${i}" type="checkbox" onchange="checking(this)">
+        <h6 class="flex-grow-1">${itemList[i]['task']}</h6>
+        <button class="btn btn-outline-light bg-warning b"  type="button" id="e${i}" onclick="editTask(this)">Edit</button>
+        <button class="btn btn-outline-light bg-danger b"  type="button" id="d${i}" onclick="removeTask(this)">Remove</button> </li>`;
 
     }
-    
+    ulTag.innerHTML=listofTask;
+    for(let i=0; i<itemList.length; i++)
+        document.getElementById(`c${i}`).checked=itemList[i]['done'];  
+}
+
+function removeTask(currElement){
+    let id=currElement.id[1];
+    itemList.splice(id,1);
+    localStorage.setItem('list',JSON.stringify(itemList));
+    display();
+}
+
+function editTask(currElement){
+    if(currElement.textContent =='Done')
+    {
+        currElement.textContent='Edit';
+        let elementData=currElement.previousElementSibling.value;
+        let newInput= document.createElement('h6');
+        newInput.textContent=elementData;
+        newInput.className='flex-grow-1';
+        currElement.parentElement.replaceChild(newInput, currElement.previousElementSibling);
+        let id=currElement.id[1];
+        itemList[id]['task']=elementData;
+        localStorage.setItem('list',JSON.stringify(itemList));
+        display();
+    }
+    else{
+        currElement.textContent="Done";
+        let elementData = currElement.previousElementSibling.textContent;
+        let newInput = document.createElement('input');
+        newInput.type="text";
+        newInput.className = "form-control";
+        newInput.placeholder="";
+        newInput.value = elementData;
+        currElement.parentElement.replaceChild(newInput, currElement.previousElementSibling);
+    }
+}
+
+function checking(currElement)
+{
+    let id=currElement.id[1];
+    if(currElement.checked==true)
+    {
+        itemList[id]['done']=true;
+    }
+    else
+    {
+        itemList[id]['done']=false;
+    }
+    localStorage.setItem('list',JSON.stringify(itemList));
+    display();
+}
